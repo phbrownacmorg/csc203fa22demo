016dcf0b932051f41f44b856b1ce00cf19336f43
Peter Brown <phbrown@acm.org>
2022-08-31 23:54:25 -0400

    Completed construction of SC interstates graph

--- SC_interstates.py
+++ SC_interstates.py
@@ -1,12 +1,28 @@
 from Vertex import Vertex
 from MR_Graph import Graph
 
-def main():
+def makeSCInterstates():
     g = Graph()
-    g.add2WayEdge('SPB', 'AVL', 65)
-    g.add2WayEdge('SPB', 'GVL', 35)
-    g.add2WayEdge('SPB', 'Clinton', 35)
+    g.add2WayEdge('SPB', 'AVL', 70)
+    g.add2WayEdge('SPB', 'GVL', 30)
+    g.add2WayEdge('SPB', 'CLN', 36)
+    g.add2WayEdge('SPB', 'CLT', 75)
+    g.add2WayEdge('GVL', 'CLN', 46)
+    g.add2WayEdge('GVL', 'ATL', 145)
+    g.add2WayEdge('CLN', 'CLB', 63)
+    g.add2WayEdge('CLB', 'CLT', 93)
+    g.add2WayEdge('CLB', 'AUG', 76)
+    g.add2WayEdge('CLB', 'FLO', 83)
+    g.add2WayEdge('CLB', 'WSL', 62)
+    g.add2WayEdge('CLB', 'WSL', 62)
+    g.add2WayEdge('WSL', 'FLO', 86)
+    g.add2WayEdge('WSL', 'SAV', 99)
+    g.add2WayEdge('WSL', 'CHS', 58)
+    g.add2WayEdge('FLO', 'FAY', 88)    
+    return g
 
+def main():
+    g = makeSCInterstates()
     for v in g:
         print(v)
     return 0
