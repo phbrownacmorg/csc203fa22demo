d88d5d31c5f8bc221f3a416bc0cdb7f303047110
Peter Brown <phbrown@acm.org>
2022-11-10 13:24:19 -0500

    Started DFA matching in Matcher.com.

--- Matcher.py
+++ Matcher.py
@@ -1,12 +1,16 @@
+from typing import Dict, Tuple
+
 class Matcher:
     """Class to do pattern-matching in strings."""
 
-    def find(pattern, text):
+    @staticmethod
+    def find(pattern:str, text:str) -> int:
         """Find the first occurrence of PATTERN in TEXT, and return
         the starting index.  If PATTERN does not occur in TEXT, return -1."""
         return Matcher.simple_matcher(pattern, text)
 
-    def simple_matcher(pattern, text):
+    @staticmethod
+    def simple_matcher(pattern:str, text:str) -> int:
         i = j = 0
 
         while True:
@@ -21,3 +25,33 @@ class Matcher:
 
             if (i + (len(pattern)-1)) == len(text): # Didn't find the pattern
                 return -1    
+
+    @staticmethod
+    def _find_next_state(pattern, state, symbol) -> int:
+        for i in range(state+1, 0, -1):
+            if (pattern[:state] + symbol)[:-i] == pattern[:i]:
+                break
+        return i
+
+    @staticmethod
+    def _make_DFA(pattern, alphabet) -> Dict[Tuple[int, str], int]:
+        DFA: Dict[Tuple[int, str], int] = {}
+        for state in range(len(pattern)):
+            for symbol in alphabet:
+                key: Tuple[int, str] = (state, symbol)
+                DFA[key] = Matcher._find_next_state(pattern, state, symbol)
+        return DFA
+
+    @staticmethod
+    def DFA_matcher(pattern: str, text: str, alphabet: str) -> int:
+        DFA: Dict[Tuple[int, str], int] = Matcher._make_DFA(pattern, alphabet)
+        state: int = 0
+        i: int = 0
+        result = -1
+        while i < len(text) and state < len(pattern):
+            key = (state, text[i])
+            state = DFA[key]
+            i = i + 1
+        if state == len(pattern):
+            result = (i - len(pattern))
+        return result
--- TestMatcher.py
+++ TestMatcher.py
@@ -35,6 +35,25 @@ class TestMatcher(unittest.TestCase):
         self.assertEqual(self._bookText.find(self._repeatN),
                         Matcher.simple_matcher(self._repeatN, self._bookText))
 
+    def test_DFA_present(self):
+        self.assertEqual(self._bookText.find(self._ACATA),
+                        Matcher.DFA_matcher(self._ACATA, self._bookText, 'ACGT'))
+        self.assertEqual(self._bookText3.find(self._repeat2),
+                        Matcher.DFA_matcher(self._repeat2, self._bookText3, 'ACGT'))
+        self.assertEqual(self._bookText.find(self._ACACAT),
+                        Matcher.DFA_matcher(self._ACACAT, self._bookText, 'ACGT'))
+        self.assertEqual(self._t8a1.find(self._repeatN),
+                        Matcher.DFA_matcher(self._repeatN, self._t8a1, 'ACGT'))
+
+    def test_DFA_absent(self):
+        self.assertEqual(self._bookText2.find(self._ACATA),
+                        Matcher.DFA_matcher(self._ACATA, self._bookText2, 'ACGT'))
+        self.assertEqual(self._bookText2.find(self._repeat2),
+                        Matcher.DFA_matcher(self._repeat2, self._bookText2, 'ACGT'))
+        self.assertEqual(self._bookText3.find(self._ACACAT),
+                        Matcher.DFA_matcher(self._ACACAT, self._bookText3, 'ACGT'))
+        self.assertEqual(self._bookText.find(self._repeatN),
+                        Matcher.DFA_matcher(self._repeatN, self._bookText, 'ACGT'))
 
 if __name__ == '__main__':
     unittest.main()
